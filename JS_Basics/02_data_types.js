30c5e2fe2acf774103618dbf0de9a07ebf3ecc27
srivastavk <91872070+srivastavk@users.noreply.github.com>
2024-08-07 20:08:13 +0000

    diff-diff data types

---
+++ JS_Basics/02_data_types.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert("hello"); // we are using nodejs, not browser
+
+/* 
+    Data Types
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => 
+    symbol =>
+
+
+    
+    // object    
+            
+*/
+
+console.log(typeof 5);
+
+// console.log(typeof null);
+
+// console.log(typeof undefined);
+
