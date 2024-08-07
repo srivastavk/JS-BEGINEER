4569d4c8d88901ec9632ac8c73d2ba877e400d07
srivastavk <91872070+srivastavk@users.noreply.github.com>
2024-08-07 19:47:04 +0000

    push

---
+++ JS_Basics/01_variable.js
@@ -0,0 +1,24 @@
+const account_id = 144352;
+let account_email = "297srivastav@gmail.com"
+var account_password = "234324";
+let account_city = "banka";
+
+let account_state;
+
+// account_id = 2;
+
+account_email = "srivastavkumar06@gmail.com"
+account_password = "92343";
+account_city = "bhagalpur";
+
+
+
+console.log(account_id);
+
+/* 
+    Prefer not to use var
+    because of issue in block scope and functional scope.
+*/
+
+
+console.table([account_id, account_email, account_password, account_city, account_state])
