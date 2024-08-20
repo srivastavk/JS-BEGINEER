dbed04246ed425a09633b4b853c68f6cbf749d5e
srivastavk <91872070+srivastavk@users.noreply.github.com>
2024-08-20 11:38:21 +0000

    Conversions of dataTypes

---
+++ JS_Basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score = "33abd";
+
+// let score = null;
+// let score = undefined;
+
+/* 
+console.log(typeof score);
+console.log(typeof(score)); 
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+
+
+/* 
+   "33" => 33
+   "33abc" (convert Number )  => (output NaN) NaN
+
+*/
+
+
+// let isLoggedIn = 1; // true
+// let isLoggedIn = ""; // false
+// let isLoggedIn = "abcd"; // true
+
+/* let booleanIsLoggedIn  = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); */
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
