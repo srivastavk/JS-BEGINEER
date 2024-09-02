f4c06e2032817be2065e02da797f66e06c493520
srivastavk <91872070+srivastavk@users.noreply.github.com>
2024-09-02 18:37:40 +0000

    Commit

---
+++ JS_Basics_1/04_objects.js
@@ -0,0 +1,78 @@
+// const tinderUser = {}  
+
+
+// Oject singleton
+
+const tinderUser = new Object();
+
+
+tinderUser.id = "234abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname :{
+            firstname : "Srivastav",
+            lastname : "Kumar"
+        }
+    }
+}
+
+/* console.log(regularUser["fullname"]);
+console.log(regularUser.fullname); */
+
+// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.lastname);
+
+
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+
+const obj2 = {
+    3 : "a",
+    4 : "b"
+}
+
+/* const obj3 = {obj1, obj2};
+console.log(obj3); */
+
+//                         target, source  
+// const obj3 = Object.assign({}, obj1, obj2);
+
+
+/* const obj3 = {...obj1, ...obj2};
+console.log(obj3); */
+
+
+// when value comming from database
+
+const users  = [
+    {
+        id : 1,
+        email : "sri2343@gmail.com"
+    },
+
+    {
+        id : 1,
+        email : "sri2343@gmail.com"
+    },
+]
+
+// console.log(users[1].email);
+
+/* console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));   // dout
+
+console.log(Object.values(tinderUser)); */
+
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
