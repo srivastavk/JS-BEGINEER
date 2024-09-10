592cd85667f3001cb5346dc7f9214c799c7a324d
srivastavk <91872070+srivastavk@users.noreply.github.com>
2024-09-10 16:25:30 +0000

    Commit

---
+++ 04_Iterations/two.js
@@ -0,0 +1,31 @@
+/* 
+    while, do-while loop
+*/
+
+
+// ****  While-Loop **** 
+
+/* let index = 0;
+while (index<=10) {
+    console.log(`Value of index is ${index}`);
+    index = index+2;
+} */
+
+/* let myArray = ["flash", "batMan", "superMan"];
+
+let index = 0;
+while(index<myArray.length)
+{
+    console.log(`Value is ${myArray[index++]}`);
+} */
+
+
+// ****** do-While loop ******
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`)
+    // score++;
+} while (score++<=10);
+
+
